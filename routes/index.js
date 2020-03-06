5e9ffd3bc804d60e36e4bd95a4817756cc9fac6c
Rachel <rsluk@ucsd.edu>
2020-03-05 18:18:45 -0800

    Need to render page A and B properly

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,11 @@ var data = require('../data.json');
 
 exports.view = function(req, res){
        res.render('index', data);
-       data['viewAlt'] = false;
+       data["viewAlt"] = false;
 
 };
 
 exports.viewAlt = function(req, res){
        res.render('index', data);
-       data['viewAlt'] = true;
+       data["viewAlt"] = true;
 };
